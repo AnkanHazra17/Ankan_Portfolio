d154493af5fee279c60b958199aab6997b37e1f5
AnkanHazra17 <ankan.hazra1710@gmail.com>
2023-09-16 14:29:07 +0530

    Try2

--- src/contents/Content.js
+++ src/contents/Content.js
@@ -73,10 +73,10 @@ const technologies = [
     //     name: "TypeScript",
     //     icon: typescript,
     // },
-    // {
-    //     name: "React JS",
-    //     icon: reactjs,
-    // },
+    {
+        name: "React JS",
+        icon: reactjs,
+    },
     {
         name: "Redux Toolkit",
         icon: redux,

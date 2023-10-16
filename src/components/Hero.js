18fab52498678cb3885b7fafc221ab3e29ac9ca4
AnkanHazra17 <ankan.hazra1710@gmail.com>
2023-10-16 18:31:31 +0530

    Commit changes

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,31 +1,31 @@
-
-import React from 'react'
-import { styles } from '../styles'
-import ComputerCanvas from './canvas/Computers'
+import React from "react";
+import { styles } from "../styles";
+import ComputerCanvas from "./canvas/Computers";
 
 const Hero = () => {
-    return (
-        <section className='w-full h-screen -z-40'>
-            <div className='w-11/12 max-w-[1200px] mx-auto relative flex flex-row'>
-                <div className='absolute top-[100px] left-0 flex gap-4'>
+  return (
+    <section className="w-full h-screen -z-40">
+      <div className="w-11/12 max-w-[1200px] mx-auto relative flex flex-row">
+        <div className="absolute top-[100px] left-0 flex gap-4">
+          <div className="flex flex-col items-center">
+            <div className="w-5 h-5 rounded-full bg-[#915EFF]"></div>
+            <div className="w-1 h-80 violet-gradient"></div>
+          </div>
 
-                    <div className='flex flex-col items-center'>
-                        <div className='w-5 h-5 rounded-full bg-[#915EFF]'></div>
-                        <div className='w-1 h-80 violet-gradient'></div>
-                    </div>
+          <div>
+            <h1 className={`${styles.heroHeadText} text-white-100`}>
+              Hi, I'm <span className="text-[#915EFF]">Ankan Hazra</span>
+            </h1>
+            <p className={`${styles.heroSubText} text-white mt-2`}>
+              I develop 3D visuals, user interfaces and web applications
+            </p>
+          </div>
+        </div>
+      </div>
 
-                    <div>
-                        <h1 className={`${styles.heroHeadText} text-white-100`}>Hi, I'm <span className='text-[#915EFF]'>Ankan</span></h1>
-                        <p className={`${styles.heroSubText} text-white mt-2`}>I develop 3D visuals, user interfaces and web applications</p>
-                    </div>
-                </div>
-            </div>
+      <ComputerCanvas></ComputerCanvas>
+    </section>
+  );
+};
 
-            
-            <ComputerCanvas></ComputerCanvas>
-            
-        </section>
-    )
-}
-
-export default Hero
+export default Hero;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,70 +1,82 @@
-
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
-import { navLinks } from '../contents/Content';
-import { HiMenuAlt3 } from "react-icons/hi"
-import { IoClose } from "react-icons/io5"
-
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { navLinks } from "../contents/Content";
+import { HiMenuAlt3 } from "react-icons/hi";
+import { IoClose } from "react-icons/io5";
 
 const Navbar = () => {
+  const [active, setActive] = useState("");
+  const [toggle, setToggle] = useState(false);
 
-    const [active, setActive] = useState("");
-    const [toggle, setToggle] = useState(false);
+  return (
+    <div className="w-full py-5 fixed top-0 z-20 flex items-center bg-primary">
+      <div className="w-11/12 max-w-[1200px] mx-auto flex items-center justify-between">
+        <Link
+          to="/"
+          onClick={() => {
+            setActive("");
+            Window.scrollTo(0, 0);
+          }}
+          className="flex items-center"
+        >
+          <p className="text-red-400 text-[24px] font-extrabold uppercase">
+            ANKAN.
+          </p>
+        </Link>
 
-    return (
-        <div className='w-full py-5 fixed top-0 z-20 flex items-center bg-primary'>
-            <div className='w-11/12 max-w-[1200px] mx-auto flex items-center justify-between'>
-                <Link to="/" onClick={() => {
-                    setActive("")
-                    Window.scrollTo(0, 0);
-                }} className='flex items-center'>
-                    <p className='text-red-400 text-[24px] font-extrabold uppercase'>Ankan.</p>
-                </Link>
-
-                <div>
-                    <div className='hidden sm:flex gap-14 items-center'>
-                        {
-                            navLinks.map((link) => (
-                                <div key={link.id} className={`${active === link.title ? "text-white" : "text-secondary"} hover:text-white transition-all duration-100`}
-                                    onClick={() => {
-                                        setActive(link.title);
-                                    }}
-                                >
-                                    <a href={`#${link.id}`}>{link.title}</a>
-                                </div>
-                            ))
-                        }
-                    </div>
-
-                </div>
-
-                <div className='sm:hidden'>
-                    <div onClick={() => setToggle(!toggle)}>
-                        {
-                            toggle ? <IoClose className='w-[28px] h-[28px] cursor-pointer'></IoClose> : <HiMenuAlt3 className='w-[28px] cursor-pointer h-[28px]'></HiMenuAlt3>
-                        }
-                    </div>
-
-                    <div className={`${!toggle ? "hidden" : "flex"} p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-md`}>
-                        <div className='flex flex-col gap-6 items-center'>
-                            {
-                                navLinks.map((link) => (
-                                    <div key={link.id} className={`${active === link.title ? "text-white" : "text-secondary"} hover:text-white transition-all duration-100`}
-                                        onClick={() => {
-                                            setActive(link.title);
-                                            setToggle(!toggle);
-                                        }}
-                                    >
-                                        <a href={`#${link.id}`}>{link.title}</a>
-                                    </div>
-                                ))
-                            }
-                        </div>
-                    </div>
-                </div>
-            </div>
+        <div>
+          <div className="hidden sm:flex gap-14 items-center">
+            {navLinks.map((link) => (
+              <div
+                key={link.id}
+                className={`${
+                  active === link.title ? "text-white" : "text-secondary"
+                } hover:text-white transition-all duration-100`}
+                onClick={() => {
+                  setActive(link.title);
+                }}
+              >
+                <a href={`#${link.id}`}>{link.title}</a>
+              </div>
+            ))}
+          </div>
         </div>
-    )
-}
 
-export default Navbar
+        <div className="sm:hidden">
+          <div onClick={() => setToggle(!toggle)}>
+            {toggle ? (
+              <IoClose className="w-[28px] h-[28px] cursor-pointer"></IoClose>
+            ) : (
+              <HiMenuAlt3 className="w-[28px] cursor-pointer h-[28px]"></HiMenuAlt3>
+            )}
+          </div>
+
+          <div
+            className={`${
+              !toggle ? "hidden" : "flex"
+            } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-md`}
+          >
+            <div className="flex flex-col gap-6 items-center">
+              {navLinks.map((link) => (
+                <div
+                  key={link.id}
+                  className={`${
+                    active === link.title ? "text-white" : "text-secondary"
+                  } hover:text-white transition-all duration-100`}
+                  onClick={() => {
+                    setActive(link.title);
+                    setToggle(!toggle);
+                  }}
+                >
+                  <a href={`#${link.id}`}>{link.title}</a>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
